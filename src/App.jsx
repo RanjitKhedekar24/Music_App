2b6560495f991269f020f44add0fbb80c1d4050d
Ranjit Khedekar <khedekaranjit5@gmail.com>
2025-04-28 18:28:59 +0530

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import {Playlist} from "./pages/Playlist";
-import {Search} from "./pages/Search";
+import { Playlist } from "./pages/Playlist";
+import { Search } from "./pages/Search";
 import Nav from "./Component/Nav";
 import { Like } from "./pages/Like";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/music-app">
+      <BrowserRouter 
+        basename="/music-app" 
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }} // Add future flags here
+      >
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/playlist" element={<Playlist />} />
-          <Route path="/like" element={<Like/>} />
+          <Route path="/like" element={<Like />} />
           <Route path="/search" element={<Search />} />
         </Routes>
       </BrowserRouter>
